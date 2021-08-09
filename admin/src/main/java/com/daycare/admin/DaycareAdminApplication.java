8dd6e55993ca2d410a05d62d3914fecfe89e46db
Vincent Concepcion <vincent22000@hotmail.com>
2021-08-09 16:54:27 -0400

    Files push

---
+++ admin/src/main/java/com/daycare/admin/DaycareAdminApplication.java
@@ -0,0 +1,13 @@
+package com.daycare.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaycareAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaycareAdminApplication.class, args);
+	}
+
+}
---
+++ admin/src/test/java/com/daycare/admin/DaycareAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daycare.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaycareAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
