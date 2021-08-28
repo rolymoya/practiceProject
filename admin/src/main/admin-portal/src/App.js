40dc2b22c454f36f94cf10f6da0d2eca755ed6a1
Vincent Concepcion <vincent22000@hotmail.com>
2021-08-28 14:46:41 -0400

    Added axios and started to connect frontend to backend

--- admin/src/main/admin-portal/src/App.js
+++ admin/src/main/admin-portal/src/App.js
@@ -1,23 +1,29 @@
+import React, {useState, useEffect} from "react";
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+
+
+
+const Children = () => {
+
+  const fetchChildren = () => {
+    axios.get().then(res => {
+      console.log(res);
+    });
+  }
+
+useEffect(() => {
+  fetchChildren();
+}, []);
+
+  return <h1>Hello</h1>
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Children />
     </div>
   );
 }
